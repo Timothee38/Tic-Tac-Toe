f472c4ae3689ef2d570ae17c82688732ff359fe9
Timothee38 <timothee.craig@gmail.com>
2016-06-20 17:53:03 +0200

    Add window extending application

--- src/fr/timothee/craig/gui/Window.java
+++ src/fr/timothee/craig/gui/Window.java
@@ -1,7 +1,29 @@
 package fr.timothee.craig.gui;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Timothee on 06/06/2016.
  */
-public class Window {
+public class Window extends Application {
+
+    public void open() {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(new Scene(createGrid()));
+        primaryStage.show();
+    }
+
+    private Group createGrid() {
+        Group group = new Group();
+        //Todo here: Create the grid
+        return group;
+    }
 }
